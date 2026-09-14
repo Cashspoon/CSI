93a9c72295317d15e5f3abc36ce64df37e77ee8a
Cashspoon <caspianjvb@gmail.com>
2026-09-13 20:28:37 -0400

    Fixed cellsToVisit being floopded with every cell

--- gerrymander.dart
+++ gerrymander.dart
@@ -49,57 +49,67 @@ class Grid
 
     void GenerateDistricts()
     {
-        int districtAmount = gridSize - 1;
+      int districtAmount = gridSize - 1;
 
-        List<(int,int)> visitedCells = [];
-        List<(int,int)> cellsToVisit = [];
+      List<(int,int)> visitedCells = [];
+      List<(int,int)> cellsToVisit = [];
 
-        for (int districtNum = 1; districtNum < districtAmount + 1; districtNum++)
+      for (int districtNum = 1; districtNum < districtAmount + 1; districtNum++)
+      {
+        //Find first empty (-1) cell
+        for (int j = 0; j < grid.length; j++)
         {
-          //Find first empty (-1) cell
-          for (int j = 0; j < grid.length; j++)
-          {
-              for (int k = 0; k < grid[j].length; k++)
-              {
-                  if (grid[j][k] == -1)
-                  {
-                      cellsToVisit.add((j,k));
-                  }
-              }
-          }
-
-          int fill = gridSize;
-          while (!cellsToVisit.isEmpty && fill > 0)
-          {
-            int y = cellsToVisit[0].$1;
-            int x = cellsToVisit[0].$2;
-
-            if (grid[y][x] == -1)
+            for (int k = 0; k < grid[j].length; k++)
             {
-              grid[y][x] = districtNum;
+                if (grid[j][k] == -1)
+                {
+                    cellsToVisit.add((j,k));
+                    break;
+                }
             }
 
-            if (!cellsToVisit.contains( (y, x + 1) ) && x + 1 < gridSize && (grid[y][x + 1] == -1 || grid[y][x + 1] == 0) )
+            if (!cellsToVisit.isEmpty)
             {
-              print("Adding cell to the right ($y,${x + 1})");
-              cellsToVisit.add((y,x + 1));
+                break;
             }
-            if (!cellsToVisit.contains( (y + 1, x) ) && y + 1 < gridSize && (grid[y + 1][x] == -1 || grid[y + 1][x] == 0) )
-            {
-              print("Adding cell to the bottom (${y + 1},$x)");
-              cellsToVisit.add((y + 1,x));
-            }
-            if (!cellsToVisit.contains( (y, x - 1) ) && x - 1 >= 0 && (grid[y][x - 1] == -1 || grid[y][x - 1] == 0) )
-            {
-              print("Adding cell to the left ($y,${x - 1})");
-              cellsToVisit.add((y,x - 1));
-            }
-
-            visitedCells.add(cellsToVisit[0]);
-            cellsToVisit.removeAt(0);
-            fill--;
-          }
         }
+
+        int fill = gridSize;
+        while (!cellsToVisit.isEmpty && fill > 0)
+        {
+            print("Visiting new cell from $cellsToVisit");
+
+          int y = cellsToVisit[0].$1;
+          int x = cellsToVisit[0].$2;
+
+          if (grid[y][x] == -1)
+          {
+            grid[y][x] = districtNum;
+          }
+
+          print("Visiting cell ${cellsToVisit[0]}");
+          if (!cellsToVisit.contains( (y, x + 1) ) && x + 1 < gridSize && grid[y][x + 1] < 1)
+          {
+            print("Adding cell to the right ($y,${x + 1})");
+            cellsToVisit.add((y,x + 1));
+          }
+          if (!cellsToVisit.contains( (y + 1, x) ) && y + 1 < gridSize && grid[y + 1][x] < 1 )
+          {
+            print("Adding cell bellow (${y + 1},$x)");
+            cellsToVisit.add((y + 1,x));
+          }
+          if (!cellsToVisit.contains( (y, x - 1) ) && x - 1 >= 0 && grid[y][x - 1] < 1 )
+          {
+            print("Adding cell to the left ($y,${x - 1})");
+            cellsToVisit.add((y,x - 1));
+          }
+
+          visitedCells.add(cellsToVisit[0]);
+          cellsToVisit.removeAt(0);
+          fill--;
+        }
+        print("District finished filling.");
+      }
     }
 
     @override
